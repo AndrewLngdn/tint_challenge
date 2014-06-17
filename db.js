7efda242793389309e8b089af3dbb371601fad5e
Andrewlngdn <andrewlngdn@gmail.com>
2014-06-16 22:52:21 -0500

    reformatted dates to look like twitters

--- db.js
+++ db.js
@@ -5,8 +5,10 @@ var battleSchema = new Schema({
 	tag1: String,
 	tag2: String,
 	tag1_count: Number,
-	tag2_count: Number
+	tag2_count: Number,
+	created_at: String
 });
+
 
 mongoose.model('Battle', battleSchema);
 mongoose.connect('mongodb://localhost:27017');
--- routes/battles.js
+++ routes/battles.js
@@ -6,21 +6,23 @@ var Battle = mongoose.model('Battle');
 /* GET users listing. */
 router.get('/', function(req, res) {
   Battle.find(function(err, battles, count){
-  	console.log(battles);
   	res.render('index', {
   		battles: battles
-
   	})
   })
 });
 
 router.post('/create', function(req, res){
-	console.log(req.body);
+ 	var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; //Months are zero based
+    var curr_year = d.getFullYear();
 	new Battle({
 		tag1: req.body.tag1,
 		tag1_count: 0,
 		tag2: req.body.tag2,
-		tag2_count: 0
+		tag2_count: 0,
+		created_at: curr_year + "-" + curr_month + "-" + curr_date
 	}).save(function(err, battle, count){
 		console.log("error: " + err);
 		console.log("battle: " + battle);
